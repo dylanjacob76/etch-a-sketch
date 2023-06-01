fd0c1740a25ef4076ad1f3d55f346339db6782a6
Miyamoto Musashi <business.dmjacob@gmail.com>
2023-06-01 10:21:21 -0400

    Refactored create grid function. It now works much more dynamically

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,21 @@
-// loop through div 256(16*16) times *DONE*
-// create a new element every time it loops *DONE*
-// append that element to the grid *DONE*
-// set a width to the grid itself *DONE*
-// use flexbox wrap to style and make the grid not just one line *DONE*
-
-// add hover effect to the gridSqaure class that changes the background color
-// aquamarine
-
-
-
-
-const createGrid = () => {
-  const grid = document.querySelector("#grid");
-
-  for (let i = 0; i < 256; i++) {
-    let sqaure = document.createElement("div");
-    sqaure.classList.add("gridSqaure");
-    sqaure.addEventListener("mouseover", function() {
-      addHover(sqaure);
-    });
-    grid.appendChild(sqaure);
-
+const createGrid = (size) => {
+  const container = document.querySelector("#container");
+  let gridSize = size * size;
+  let sqaureSize = 500/size;
+  for (let i = 0; i < gridSize; i++) {
+    const sqaure = document.createElement("div");
+    sqaure.classList.add("gridSquare");
+    sqaure.setAttribute("style", `width: ${sqaureSize}px; height: ${sqaureSize}px`)
+    sqaure.addEventListener("mouseover", () => createGridHover(sqaure))
+    container.appendChild(sqaure);
   }
 }
 
-const addHover = (square) => {
-  square.classList.add("active");
+const createGridHover = (sqaure) => {
+  sqaure.classList.add("active");
 }
 
-createGrid();
+createGrid(16);
+
+
+// create a button that takes in the createGrid function as a callback and makes the size parameter equal to the prompt
